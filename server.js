b710f76e8a7dbe67efd7ef24ed89f14166d7f80f
mahmoud hassan <heshamalamam73@gmail.com>
2020-11-01 21:03:27 +0100

    11

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.use("/api/products", producRoute);
 if (process.env.NODE_ENV) {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
   });
 }
 
